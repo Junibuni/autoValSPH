8113272e998a6cbe1f89c56e5a674c639d7ddef9
junibuni <rich4103@naver.com>
2025-01-24 11:22:06 +0900

    parameter 하드코딩으로 변경

--- validation_test/obc_poiseuille.py
+++ validation_test/obc_poiseuille.py
@@ -2,9 +2,13 @@ import numpy as np
 
 from validation_test.utils.interpolate_and_plot_line import interpolate_and_plot_line
 
-def run(vtk_folder, save_log_pth, grid_number, start_point, end_point, n_points, data_array_name, radius):
-    start_point = np.array(start_point)
-    end_point = np.array(end_point)
+def run(vtk_folder, save_log_pth, grid_number):
+    radius =  0.002,
+    n_points =  1000,
+    start_point =  np.array([0.5, -0.05, 0.0]),
+    end_point =  np.array([0.5, 0.05, 0.0]),
+    data_array_name =  "velocity",
+    xyz =  "x"
 
     distances, values = interpolate_and_plot_line(
         vtk_folder=vtk_folder, 
@@ -13,6 +17,7 @@ def run(vtk_folder, save_log_pth, grid_number, start_point, end_point, n_points,
         end_point=end_point, 
         n_points=n_points, 
         data_array_name=data_array_name, 
-        radius=radius)
+        radius=radius,
+        xyz=xyz)
     
     # 후처리 이후 save_log_pth에 결과 저장 (csv)
